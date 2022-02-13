eb76ab3a2f009cd84a370f2bef106ff30b178758
Yassine <youngkesinova@gmail.com>
2022-02-13 16:21:50 +0100

    Update live.js

--- live.js
+++ live.js
@@ -2,7 +2,7 @@ const Audic = require("audic");
 
 const mp3 = new Audic("https://server11.mp3quran.net/yasser/002.mp3");
 
-return await mp3
+return mp3
   .play()
   .then(() => {
     console.log(`Audio is playing now!`);

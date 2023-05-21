32a8f8832be223b757aea1828042ba7ef42bb9c1
Yassine <youngkesinova@gmail.com>
2023-05-21 13:51:45 +0200

    Update Play

---
+++ play.py
@@ -0,0 +1,56 @@
+import pygame
+import keyboard
+import requests
+import os
+
+
+def play_audio(file_path):
+    pygame.mixer.init()
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
+
+
+def download_mp3(url):
+    response = requests.get(url)
+    file_path = "audio.mp3"
+
+    with open(file_path, "wb") as file:
+        file.write(response.content)
+
+    return file_path
+
+
+def main():
+    input_path = input("Enter the audio file path or online MP3 link: ")
+
+    if input_path.startswith("http://") or input_path.startswith("https://"):
+        file_path = download_mp3(input_path)
+    else:
+        file_path = input_path
+
+    if not os.path.exists(file_path):
+        print("File not found.")
+        return
+
+    play_audio(file_path)
+
+    print("Playing audio. Press 'Y' to pause, 'X' to stop.")
+
+    while pygame.mixer.music.get_busy():
+        if keyboard.is_pressed("y"):
+            pygame.mixer.music.pause()
+            print("Audio paused. Press 'Y' to resume, 'X' to stop.")
+
+        if keyboard.is_pressed("x"):
+            pygame.mixer.music.stop()
+            print("Audio stopped.")
+            break
+
+        pygame.time.Clock().tick(10)
+
+    if file_path == "audio.mp3":
+        os.remove(file_path)
+
+
+if __name__ == "__main__":
+    main()

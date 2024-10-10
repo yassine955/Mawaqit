20e6279cc506688d4e1473f02232066862c1819f
Yassine <54895428+yassine955@users.noreply.github.com>
2024-10-10 22:32:05 +0200

    New Update (integrated JSON)

--- index.py
+++ index.py
@@ -1,50 +1,38 @@
 from datetime import datetime
 import time
+from typing import Any, Dict, TypedDict
 from pygame import mixer
 import requests
 from bs4 import BeautifulSoup
+import re
+import time
+import json
+from functions.scraper import get_prayer_times_to_json, get_current_time
 
+from functions.get_config import get_config
 
-# Initialize pygame.mixer
+# CONFIG SETTINGS
 mixer.init()
+config = get_config()
+response = requests.get(config["uri"])
+adhan_sound = "audio/azan5.wav"
+
+get_prayer_times_to_json(response, config)
+
+list_of_times = get_current_time()
+
+print(list_of_times)
 
 
-# Function to parse time string to datetime object
-def parse_time(time_str):
-    return datetime.strptime(time_str, "%H:%M")
-
-
-# Function to check if current time is in the list of prayer times
 def check_prayer_times():
 
-    # Get current time
     current_time = datetime.now().strftime("%H:%M")
 
-    url = "https://www.al-yaqeen.com/gebedstijden/"
-    cookies = {"currentCity": "29263"}
-
-    response = requests.get(url, cookies=cookies)
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    # Find the specific table row containing the prayer times
-    prayer_table_row = soup.find("tr", class_="prayer-table__day current")
-
-    # Extract all the prayer times from the specified row
-    times = [td.get_text(strip=True) for td in prayer_table_row.find_all("td")]
-
-    # Remove the second element from the list
-    if len(times) >= 2:
-        del times[1]
-
-    print(times)
-
     # Check if current time is present in the list of prayer times
-    if current_time in times:
+    if current_time in list_of_times:
         print("Current time is present in the list of prayer times.")
-        mixer.music.load("azan5.wav")
+        mixer.music.load(adhan_sound)
         mixer.music.play()
-
-        # Toggle the audio file for the next call
 
     else:
         print("Current time is not present in the list of prayer times.")
@@ -54,5 +42,7 @@ while True:
 
     now = datetime.now()
     seconds_until_next_minute = 60 - now.second
+
     time.sleep(seconds_until_next_minute)
+
     check_prayer_times()

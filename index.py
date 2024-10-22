2cb3e3bbec14a114b9865fcbf2a886d5c5b6ff08
Yassine <54895428+yassine955@users.noreply.github.com>
2024-10-23 00:05:07 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 from typing import Any, Dict, TypedDict
 from pygame import mixer
@@ -29,14 +29,28 @@ def check_prayer_times():
 
     current_time = datetime.now().strftime("%H:%M")
 
+    time_obj = datetime.strptime(list_of_times[0], "%H:%M")
+    new_time_obj_minus_30 = time_obj - timedelta(minutes=40)
+    new_time_obj_minus_30_str = new_time_obj_minus_30.strftime("%H:%M")
+
     print(f"Current time: {current_time}")
     print(f"Today's Times: {list_of_times}")
+    print(f"Today's Imsaak Time: {new_time_obj_minus_30_str}")
 
     # Check if current time is present in the list of prayer times
     if current_time in list_of_times:
         print("Current time is present in the list of prayer times.")
         mixer.music.load(adhan_sound)
         mixer.music.play()
+
+    if current_time == new_time_obj_minus_30_str:
+        print(
+            f"Current Time: {current_time} -> Imsaak Time: {new_time_obj_minus_30_str}"
+        )
+        for _ in range(3):
+            mixer.music.load("audio/short.wav")
+            mixer.music.play()
+            time.sleep(15)
 
     else:
         print("Current time is not present in the list of prayer times.")
